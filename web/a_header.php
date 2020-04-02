abdf215d83e00959b00ba1f16ab64f7700ebca12
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-02 14:46:36 -0600

    A
    A

--- web/a_header.php
+++ web/a_header.php
@@ -156,7 +156,7 @@
               <ul class="standard_dropdown main_nav_dropdown">
                 <li><a href="index.php">Inicio<i class="fas fa-chevron-down"></i></a></li>
                 <li><a href="info.php">Nosotros<i class="fas fa-chevron-down"></i></a></li>
-                <li><a href="vendido.php">Lo más vendido<i class="fas fa-chevron-down"></i></a></li>
+                <li><a href="vendido.php">Tienda<i class="fas fa-chevron-down"></i></a></li>
                 <li><a href="contact.php">Contacto<i class="fas fa-chevron-down"></i></a></li>
               </ul>
             </div>
--- web/contact.php
+++ web/contact.php
@@ -1,3 +1,8 @@
+<?php
+	require_once("control_db.php");
+	$db = new Tienda();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
