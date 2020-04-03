ae26d2c40a2b19f878d4d5039d0b4d71e70978a6
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-03 11:09:19 -0600

    a
    a

--- a_jobs/existencia.php
+++ a_jobs/existencia.php
@@ -1,11 +1,15 @@
 <?php
   require_once("../control_db.php");
+  error_reporting(E_ALL);
+  ini_set('display_errors', '1');
+
   class Existencia extends Tienda{
   	public function __construct(){
   		parent::__construct();
   	}
   }
   $db = new Existencia();
+  
   $resp = crearNuevoToken();
   $tok=$resp->token;
 
@@ -13,7 +17,7 @@
   //$sql="select * from productos where timeexis<$compara order by timeexis asc limit 75";
 
   $fecha=mktime(date("H"),date("i"),date("s"),date("m"),date("d"),date("Y"));
-  $sql="select * from productos where interno=0 order by timeexis asc limit 100";
+  $sql="select * from productos where interno=0 order by timeexis asc limit 75";
   $fmodif = date("Y-m-d H:i:s");
 
   $stmt= $db->dbh->query($sql);
