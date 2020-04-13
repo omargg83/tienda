ce7b14e0fa9b316eb180d25e7adbb83c77269348
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-12 19:44:55 -0500

    a
    a

--- web/control_db.php
+++ web/control_db.php
@@ -1232,23 +1232,12 @@
 					//Tell PHPMailer to use SMTP
 					$mail->isSMTP();
 
-					//Enable SMTP debugging
-					// SMTP::DEBUG_OFF = off (for production use)
-					// SMTP::DEBUG_CLIENT = client messages
-					// SMTP::DEBUG_SERVER = client and server messages
-					//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-					//Set the hostname of the mail server
-					$mail->Host = 'smtp.gmail.com';
 					// use
 					// $mail->Host = gethostbyname('smtp.gmail.com');
 					// if your network does not support SMTP over IPv6
-
-					//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-					$mail->Port = 587;
-
-					//Set the encryption mechanism to use - STARTTLS or SMTPS
-					$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+					$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+					$mail->Host = 'smtp.gmail.com';
+					$mail->Port = 465;
 
 					//Whether to use SMTP authentication
 					$mail->SMTPAuth = true;
