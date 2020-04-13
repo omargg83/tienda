4b150847f1bffab212dbd34fcaf400b669458c91
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-12 22:01:40 -0500

    a
    a

--- web/mercado.php
+++ web/mercado.php
@@ -1,15 +1,15 @@
 <?php
 
+	$id=$_REQUEST['id'];
+	echo $id;
 
 
 
 
-/*
+	if (isset($_GET["id"], $_GET["topic"])) {
+	    http_response_code(200);
+	    return;
+	}
 
-if (isset($_GET["id"], $_GET["topic"])) {
-    http_response_code(200);
-    return;
-}
-*/
 
 ?>
