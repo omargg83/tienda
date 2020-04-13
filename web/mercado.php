ac0a920e882e80eaa81e473ff2a1b966ff38bd25
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-12 21:58:28 -0500

    a
    a

--- web/mercado.php
+++ web/mercado.php
@@ -1,56 +1,15 @@
 <?php
-	require_once("control_db.php");
-	$db = new Tienda();
-	$mercado=$db->ajustes_editar();
-	$merca=$mercado->mercado_token;
 
-  $token = $_REQUEST["token"];
-  $payment_method_id = $_REQUEST["payment_method_id"];
-  $installments = $_REQUEST["installments"];
-  $issuer_id = $_REQUEST["issuer_id"];
-	$idx = $_REQUEST["idx"];
 
-	
 
-	$ped=$db->pedido_ver($idx);
 
-	$nombre=$ped->nombre;
-	$correo=$ped->correo;
-	$total=$ped->total;
 
-  require_once 'vendor/autoload.php';
-	MercadoPago\SDK::setAccessToken($merca);
+/*
 
-  $payment = new MercadoPago\Payment();
-  $payment->transaction_amount = 133;
-	$payment->description = "TIC-SHOP";
-  $payment->token = $token;
-  $payment->installments = $installments;
-  $payment->payment_method_id = $payment_method_id;
-  $payment->issuer_id = $issuer_id;
-  $payment->payer = array(
-  "email" => $correo
-  );
+if (isset($_GET["id"], $_GET["topic"])) {
+    http_response_code(200);
+    return;
+}
+*/
 
-  $payment->save();
-  if($payment->status=="approved"){
-		$arreglo =array();
-		$arreglo+= array('estatus'=>"procesando");
-		$arreglo+= array('pago'=>"Mercado Pago");
-		$arreglo+= array('idpago'=>$payment->id);
-		$arreglo+= array('pagador'=>$payment->payer->email);
-		$arreglo+= array('estado_pago'=>$payment->status);
-		$x=$db->update('pedidos',array('id'=>$idx), $arreglo);
-
-		$ped=json_decode($x);
-		$id=$ped->id;
-		if($ped->error==0){
-
-		}
-	}
-	else{
-
-	}
-	echo $payment->status;
-
-  ?>
+?>
