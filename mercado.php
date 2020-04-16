8a3f6a5c6fbaef4d15ad5b8d49653ab65986a28c
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-15 22:38:53 -0500

    a
    a

--- mercado.php
+++ mercado.php
@@ -3,7 +3,6 @@
 	ini_set('display_errors', '1');
 	date_default_timezone_set("America/Mexico_City");
 
-
 	class Tienda{
 		public function __construct(){
 			date_default_timezone_set("America/Mexico_City");
@@ -40,47 +39,16 @@
 	require __DIR__ .  '/vendor/autoload.php';
 	MercadoPago\SDK::setAccessToken($mercado_token);
   $payment = MercadoPago\Payment::find_by_id($id);
-
-  echo var_dump($payment);
+  echo $payment->external_reference;
 
 
 /*
 
 
-	MercadoPago\SDK::setAccessToken($mercado_token);
-
-	$payment = MercadoPago\Payment::find_by_id($id);
-
-	echo "pago:".$payment;
-
-	$ch = curl_init('https://api.mercadopago.com/v1/payments/' . $id);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $metodo);
-
-	$json = json_encode(array('access_token' => $mercado_token));
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($json), 'x-auth: ' . $token));
-
-	curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
-	$result = curl_exec($ch);
-
-	echo $result;
-	echo "error".curl_error($ch);
-	echo curl_close($ch); // close cURL handler
-
-	return json_decode($result);
-
 	$input = @file_get_contents("php://input");
 	$texto=$input;
 	$event_json = json_decode($input);
 	$id = $event_json->data->id;
-
-
-	https://api.mercadopago.com/v1/payments/[ID]?access_token=[ACCESS_TOKEN]
-
-
-	/*
 
 	$sql="select * from pedidos where idpago='$id'";
 	$sth = $db->dbh->prepare($sql);
@@ -103,53 +71,7 @@
 
 
 /*
-	if(isset($_GET["id"]) and strlen($_GET["id"])>0){
 
-		$id=$_GET["id"];
-		$topic=$_GET["topic"];
-
-
-		$mercado=$db->ajustes_editar();
-		$mercado_token=$mercado->mercado_token;
-
-	//////////////////////////////////////////////////////////////////////////////
-		require __DIR__ .  '/vendor/autoload.php';
-		MercadoPago\SDK::setAccessToken($mercado_token);
-
-		$merchant_order = null;
-		echo $id;
-		switch(	$topic) {
-				case "payment":
-						$payment = MercadoPago\Payment::find_by_id($id);
-						$merchant_order = MercadoPago\MerchantOrder::find_by_id($payment->order->id);
-						break;
-				case "merchant_order":
-						$merchant_order = MercadoPago\MerchantOrder::find_by_id($id);
-						break;
-		}
-
-		$paid_amount = 0;
-		foreach ($merchant_order->payments as $payment) {
-				if ($payment['status'] == 'approved'){
-						$paid_amount += $payment['transaction_amount'];
-				}
-		}
-
-		// If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items
-		if($paid_amount >= $merchant_order->total_amount){
-				if (count($merchant_order->shipments)>0) { // The merchant_order has shipments
-						if($merchant_order->shipments[0]->status == "ready_to_ship") {
-								$resp="Totally paid. Print the label and release your item. $id";
-								print_r("Totally paid. Print the label and release your item. $id");
-						}
-				} else { // The merchant_order don't has any shipments
-						$resp="Totally paid. Release your item. $id";
-						print_r("Totally paid. Release your item. $id");
-				}
-		} else {
-				$resp="Not paid yet. Do not release your item. $id";
-				print_r("Not paid yet. Do not release your item. $id");
-		}
 	///////////////////////////////////////////////////////////////////////////////
 
 
