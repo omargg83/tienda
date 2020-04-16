5060a4c40c573faaaa37683124026afcb26b47e2
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-16 09:12:38 -0500

    a
    a

--- index.php
+++ index.php
@@ -395,13 +395,29 @@
 										<!-- Slider Item -->
 										<?php
 											foreach($semana as $key){
+												if($key->precio_tipo==0){
+													$preciof=$key->preciof;
+												}
+												if($key->precio_tipo==1){
+													$p_total=$key->preciof+(($key->preciof*$db->cgeneral)/100);
+													$preciof=$p_total;
+												}
+												if($key->precio_tipo==2){
+													$preciof=$key->precio_tic;
+												}
+												if($key->precio_tipo==3){
+													$p_total=$key->precio_tic+(($key->precio_tic*$db->cgeneral)/100);
+													$preciof=$p_total;
+												}
+
+
 												echo "<div class='arrivals_slider_item' >";
 												echo "<a href='product.php?id=".$key->id."'>";
 													echo "<div class='border_active'></div>
 													<div class='product_item is_new d-flex flex-column align-items-center justify-content-center text-center' >
 														<div class='product_image d-flex flex-column align-items-center justify-content-center'><img src='".$db->doc.$key->img."' alt='' width='100px'></div>
 														<div class='product_content'>
-															<div class='product_price'>".moneda($key->preciof)."</div>
+															<div class='product_price'>".moneda($preciof)."</div>
 															<div class='product_name'><div><a href='product.php?id=".$key->id."'>".$key->nombre."</a></div></div>
 															<div class='product_extras'>
 																<button class='product_cart_button' onclick='carrito(".$key->id.",1)'>Agregar al carrito</button>
