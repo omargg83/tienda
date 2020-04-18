e4a86231e6bba998c07d0fcecfc837c3d429db56
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-17 23:23:35 -0500

    a
    a

--- control_db.php
+++ control_db.php
@@ -1367,13 +1367,22 @@
 			$mail->SMTPAutoTLS = false;
 			$mail->Port = 25;
 */
+/*
 			$email->isSMTP();
 			$email->SMTPDebug = 1;
 			$email->SMTPAuth = true;
 			$email->SMTPSecure = 'tls';
 			$email->Host = "mail.tic-shop.com.mx";
 			$email->Port = 465;
+			*/
 
+			$mail->IsSMTP(); // enable SMTP
+		  $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+		  $mail->SMTPAuth = true; // authentication enabled
+		  $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+		  $mail->Host = "mail.tic-shop.com.mx";
+		  $mail->Port = 465; // or 587
+		  $mail->IsHTML(true);
 
 
 			$mail->Username = $this->ecorreo;
