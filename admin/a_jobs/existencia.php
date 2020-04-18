e5d8ecc3c43dc2ecaf2d87aff001817be94c776f
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-17 22:30:41 -0500

    a
    a

--- admin/a_jobs/existencia.php
+++ admin/a_jobs/existencia.php
@@ -52,21 +52,58 @@
   $tok=$resp->token;
 
   $fecha=mktime(date("H"),date("i"),date("s"),date("m"),date("d"),date("Y"));
-  $sql="select * from productos where interno=0 order by timeexis asc limit 50";
+  $sql="select * from productos where interno=0 order by timeexis asc limit 25";
   $fmodif = date("Y-m-d H:i:s");
 
   $stmt= $db->dbh->query($sql);
   foreach($stmt as $key){
     $clave=$key['clave'];
-
+    $id=$key['id'];
     $servicio = "existencia/$clave/TOTAL";
     $metodo="GET";
 
     $resp =servicioApi($metodo,$servicio,NULL,$tok);
     if (is_object($resp)){
       $existencia=$resp->existencia_total;
-      $sql="update productos set existencia='$existencia', timeexis='$fecha', horaexist='$fmodif' where id='".$key['id']."'";
+      $sql="update productos set existencia='$existencia', timeexis='$fecha', horaexist='$fmodif' where id='$id'";
       $stmt2= $db->dbh->query($sql);
+    }
+
+    ///////////////////////////////////////////////////////////almacen
+
+    $servicio = "existencia/$clave";
+    $metodo="GET";
+
+    $resp_a=array();
+    $resp =servicioApi($metodo,$servicio,NULL,$tok);
+    if (is_object($resp)){
+
+      $sql="delete from producto_exist where id='$id'";
+      $sth3 = $db->dbh->prepare($sql);
+      $sth3->execute();
+
+      $objectToArray = (array)$resp;
+      while (current($objectToArray)) {
+        $name=key($objectToArray);
+        $info = (array)$objectToArray[$name];
+        $valor=$info['existencia'];
+        if($valor>0){
+          $sql="select * from almacen where numero='$name'";
+          $sth4 = $db->dbh->prepare($sql);
+          $sth4->execute();
+          if($sth4->rowCount()){
+            $almax=$sth4->fetch(PDO::FETCH_OBJ);
+
+            $sql="insert into producto_exist (id, almacen, existencia) values (:id, :almacen, :existencia)";
+            $sth2 = $db->dbh->prepare($sql);
+            $sth2->bindValue(':id', $id);
+            $sth2->bindValue(':almacen', $almax->homoclave);
+            $sth2->bindValue(':existencia', $valor);
+            $sth2->execute();
+          }
+        }
+        next($objectToArray);
+      }
     }
   }
 
