5408e53e5126f38833960c6ba3f8fea917548019
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-20 14:33:45 -0500

    a
    a

--- tienda.php
+++ tienda.php
@@ -98,7 +98,7 @@
 									<div class='sidebar_title'>Categoria</div>
 									<ul class='sidebar_categories'>";
 									foreach($db->cat_ct($id) as $key2){
-										echo "<li><a href='/tienda.php?tipo=1&id=".$key2->id."'>".$key2->categoria."</a></li>";
+										echo "<li><a href='/tienda.php?tipo=2&id=".$key2->id."'>".$key2->categoria."</a></li>";
 									}
 									echo "</ul>
 									</div>";
@@ -108,7 +108,7 @@
 									<div class='sidebar_title'>Categoria</div>
 									<ul class='sidebar_categories'>";
 									 foreach($db->sub_cat($id) as $key3){
-										echo "<li><a href='/tienda.php?tipo=2&id=".$key3->id."'>".$key3->subcategoria."</a></li>";
+										echo "<li><a href='/tienda.php?tipo=3&id=".$key3->id."'>".$key3->subcategoria."</a></li>";
 									}
 									echo "</ul>
 									</div>";
