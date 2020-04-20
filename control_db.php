dce58cbb8d93fdd64bb5d7093b94448ee298a79a
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-20 14:47:17 -0500

    a
    a

--- control_db.php
+++ control_db.php
@@ -375,7 +375,11 @@
 				$sql="select productos.* from producto_cat
 							left outer join categoria_ct on categoria_ct.id=producto_cat.idcategoria_ct
 							left outer join productos on productos.categoria=categoria_ct.categoria
-							where producto_cat.idcategoria=$cat and productos.activo=1";
+							where producto_cat.idcategoria=$cat";
+							if(strlen($marca>0)){
+								$sql.=" and productos.marca='$marca'";
+							}
+				$sql=." and productos.activo=1";
 				$sth = $this->dbh->prepare($sql);
 				$sth->execute();
 				return $sth->fetchAll(PDO::FETCH_OBJ);
@@ -390,7 +394,11 @@
 				$sql="select productos.* from producto_cat
 							left outer join categoria_ct on categoria_ct.id=producto_cat.idcategoria_ct
 							left outer join productos on productos.categoria=categoria_ct.categoria
-							where producto_cat.idcategoria=$cat and productos.activo=1 group by productos.marca";
+							where producto_cat.idcategoria=$cat";
+							if(strlen($marca>0)){
+								$sql.=" and productos.marca='$marca'";
+							}
+				$sql=." and productos.activo=1";
 				$sth = $this->dbh->prepare($sql);
 				$sth->execute();
 				return $sth->fetchAll(PDO::FETCH_OBJ);
