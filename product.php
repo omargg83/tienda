b204013f1059c5f6a480d35c5d5b40ab554ada20
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-20 11:00:49 -0500

    a
    a

--- product.php
+++ product.php
@@ -366,27 +366,27 @@
 
 </div>
 
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="styles/bootstrap4/popper.js"></script>
-<script src="styles/bootstrap4/bootstrap.min.js"></script>
-<script src="plugins/greensock/TweenMax.min.js"></script>
-<script src="plugins/greensock/TimelineMax.min.js"></script>
-<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
-<script src="plugins/greensock/animation.gsap.min.js"></script>
-<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
-<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-<script src="plugins/easing/easing.js"></script>
-<script src="js/product_custom.js"></script>
+<script src="./js/jquery-3.3.1.min.js"></script>
+<script src="./styles/bootstrap4/popper.js"></script>
+<script src="./tyles/bootstrap4/bootstrap.min.js"></script>
+<script src="./plugins/greensock/TweenMax.min.js"></script>
+<script src="./plugins/greensock/TimelineMax.min.js"></script>
+<script src="./plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="./plugins/greensock/animation.gsap.min.js"></script>
+<script src="./plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="./plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="./plugins/easing/easing.js"></script>
+<script src="./js/product_custom.js"></script>
 
 <!--   Alertas   -->
-<script src="librerias15/swal/dist/sweetalert2.min.js"></script>
-<link rel="stylesheet" href="librerias15/swal/dist/sweetalert2.min.css">
+<script src="./librerias15/swal/dist/sweetalert2.min.js"></script>
+<link rel="stylesheet" href="./librerias15/swal/dist/sweetalert2.min.css">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
-<script src="sagyc.js"></script>
+<script src="./sagyc.js"></script>
 
 <script>
 $(document).ready(function(){
