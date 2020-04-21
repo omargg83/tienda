6559b6471c9586f5feb05032c61ac0155ae58165
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-21 00:06:01 -0500

    a
    a

--- control_db.php
+++ control_db.php
@@ -1440,19 +1440,19 @@
 			$email->Port = 465;
 			//	  $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 			*/
-			/*
-						$mail->IsSMTP(); // enable SMTP
-					  $mail->SMTPAuth = true; // authentication enabled
-					  $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-					  $mail->Host = "mail.tic-shop.com.mx";
-					  $mail->Port = 465; // or 587
-					  $mail->IsHTML(true);
 
-						$mail->Username = $this->ecorreo;
-						$mail->Password = $this->Password;
-						$mail->setFrom("admin@tic-shop.com.mx", 'TIC-SHOP');
-			*/
+				$mail->IsSMTP(); // enable SMTP
+			  $mail->SMTPAuth = true; // authentication enabled
+			  $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+			  $mail->Host = "mail.tic-shop.com.mx";
+			  $mail->Port = 465; // or 587
+			  $mail->IsHTML(true);
 
+				$mail->Username = $this->ecorreo;
+				$mail->Password = $this->Password;
+				$mail->setFrom("admin@tic-shop.com.mx", 'TIC-SHOP');
+
+/*
 			$mail->IsSMTP(); // enable SMTP
 			$mail->SMTPAuth = true; // authentication enabled
 			$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
@@ -1464,7 +1464,7 @@
 			$mail->Password = "TEUFEL123";                       // SMTP password                                // TCP port to connect to
 			$mail->CharSet = 'UTF-8';
 			$mail->setFrom('sistema.subsaludpublicahgo@gmail.com', 'sistema.subsaludpublicahgo@gmail.com');
-
+*/
 
 			$mail->addAddress($correo);
 			$mail->addCC("admin@tic-shop.com.mx");
