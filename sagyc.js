64aef0a67db17cbb46ddf6f8e863fdf7edbbff13
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-21 09:05:05 -0500

    a
    a

--- sagyc.js
+++ sagyc.js
@@ -666,44 +666,22 @@ $(document).on('submit','#carrito_form',function(e){
 
     function initPriceSlider() {
     	if($("#slider-range").length){
-
-    	$("#slider-range").slider({
-				range: true,
-				min: 0,
-				max: 1000,
-				values: [ 0, 580 ],
-				slide: function( event, ui )
-				{
-
-					$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-				}
-			});
-
-			$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) + " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-
-			$('.ui-slider-handle').on('mouseup', function(){
-				$('.product_grid').isotope({
-		            filter: function(){
-
-
-                var priceRange = $('#amount').val();
-			        	var priceMin = parseFloat(priceRange.split('-')[0].replace('$', ''));
-			        	var priceMax = parseFloat(priceRange.split('-')[1].replace('$', ''));
-
-                alert(priceMin + "-" + priceMax);
-
-
-			        	var itemPrice = $(this).find('.product_price').clone().children().remove().end().text().replace( '$', '' );
-
-			        	return (itemPrice > priceMin) && (itemPrice < priceMax);
-		            },
-		            animationOptions: {
-		                duration: 750,
-		                easing: 'linear',
-		                queue: false
-		            }
-		        });
-			});
+        $( function() {
+            $( "#slider-range" ).slider({
+              range: true,
+              min: 0,
+              max: 500,
+              values: [ 75, 300 ],
+              slide: function( event, ui ) {
+                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+              },
+              change: function( event, ui ) {
+              alert(ui.values[ 0 ] + " - " + ui.values[ 1 ]);
+              }
+            });
+            $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+              " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+          } );
 
 
     	}
