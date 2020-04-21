d291949c717be3fc1c715586cc9d88dbb8ebe7e6
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-20 20:56:30 -0500

    a
    a

--- tienda.php
+++ tienda.php
@@ -247,10 +247,12 @@
 							<nav aria-label="Page navigation example">
 							  <ul class="pagination">
 									<?php
-										echo "<li class='page-item'><a class='page-link active' href='tienda.php?tipo=$tipo&id=$id&pag=0&marcaf=$marcaf'>Primera</a></li>";
+										echo "<li class='page-item'><a class='page-link' href='tienda.php?tipo=$tipo&id=$id&pag=0&marcaf=$marcaf'>Primera</a></li>";
 										for($i=0; $i<$num_paginas;$i++){
 											$t=$i+1;
-											echo "<li class='page-item'><a class='page-link' href='tienda.php?tipo=$tipo&id=$id&pag=$i&marcaf=$marcaf'>".$t."</a></li>";
+											echo "<li class='page-item";
+												if($i==$pag){ echo " active ";
+											echo ""'><a class='page-link' href='tienda.php?tipo=$tipo&id=$id&pag=$i&marcaf=$marcaf'>".$t."</a></li>";
 										}
 										$t=ceil($num_paginas)-1;
 										echo "<li class='page-item'><a class='page-link' href='tienda.php?tipo=$tipo&id=$id&pag=$t&marcaf=$marcaf'>Ultima</a></li>";
