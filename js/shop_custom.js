bd3f38ca16e1128f7198aa941a87af5bcf71c061
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-20 23:17:28 -0500

    a

--- js/shop_custom.js
+++ js/shop_custom.js
@@ -358,6 +358,27 @@ $(document).ready(function()
 
 	*/
 
+    function initPriceSlider() {
+    	if($("#slider-range").length){
+
+    	$("#slider-range").slider({
+				range: true,
+				min: 0,
+				max: 1000,
+				values: [ 0, 580 ],
+				slide: function( event, ui )
+				{
+					$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+				}
+			});
+
+			$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) + " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
+
+
+
+    	}
+    }
 
     /*
 
