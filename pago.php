7c3b302ead47aee127c8ce157886bbde3253bdec
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-21 18:53:08 -0500

    a
    a

--- pago.php
+++ pago.php
@@ -346,6 +346,51 @@
 					  </script>
 					</form>
 
+					<script
+					   src="https://www.paypal.com/sdk/js?client-id=<?php echo $paypal_client; ?>&currency=MXN" >
+					 </script>
+
+					 <div id="paypal-button-container"></div>
+
+					 <script>
+					    paypal.Buttons({
+					    createOrder: function(data, actions) {
+					      // This function sets up the details of the transaction, including the amount and line item details.
+					      return actions.order.create({
+					        purchase_units: [{
+					          amount: {
+					            value: '<?php echo $gtotal; ?>'
+					          }
+					        }]
+					      });
+					    },
+					    onApprove: function(data, actions) {
+					      Swal.fire({
+					          type: 'success',
+					          title: 'no cierre la ventana, finalizando pago',
+					          showConfirmButton: false
+					      });
+					      return actions.order.capture().then(function(details) {
+					        $.ajax({
+					          url: "paypal-transaction-complete.php",
+					          type: "POST",
+					          data: {
+					            "id":details.id,
+					            "mail":details.payer.email_address,
+					            "estatus":details.status,
+					            "idx":<?php echo $idpedido; ?>
+					          },
+					          success: function( response ) {
+
+					          }
+					        });
+					        alert('Transaction completed by ' + details.payer.name.given_name);
+					      });
+					    }
+					  }).render('#paypal-button-container');
+					  //This function displays Smart Payment Buttons on your web page.
+
+					 </script>
 
 				</div>
 			</div>
