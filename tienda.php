81bcc75be1b4063747305053466cffff3082a1e7
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-20 21:16:18 -0500

    a
    a

--- tienda.php
+++ tienda.php
@@ -130,18 +130,21 @@
 		</div>
 	</div>
 
-<nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="#">Home</a></li>
-    <li class="breadcrumb-item"><a href="#">Library</a></li>
-    <li class="breadcrumb-item active" aria-current="page">Data</li>
-  </ol>
-</nav>
 
 
 	<!-- Shop -->
 	<div class="shop">
 		<div class="container">
+
+			<nav aria-label="breadcrumb">
+			  <ol class="breadcrumb">
+			    <li class="breadcrumb-item"><a href="#">Home</a></li>
+			    <li class="breadcrumb-item"><a href="#">Library</a></li>
+			    <li class="breadcrumb-item active" aria-current="page">Data</li>
+			  </ol>
+			</nav>
+
+
 			<div class="row">
 				<div class="col-lg-3">
 
