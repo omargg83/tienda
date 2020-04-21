b8ee9c42a9585ac66a3990a19e8e0491f4666e93
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-20 20:45:03 -0500

    a
    a

--- tienda.php
+++ tienda.php
@@ -252,8 +252,9 @@
 										for($i=0; $i<$num_paginas;$i++){
 											$t=$i+1;
 											echo "<li class='page-item'><a class='page-link' href='tienda.php?tipo=$tipo&id=$id&pag=$i&marcaf=$marcaf'>".$t."</a></li>";
-										} 
-										echo "<li class='page-item'><a class='page-link' href='tienda.php?tipo=$tipo&id=$id&pag=$i&marcaf=$marcaf'>Ultima</a></li>";
+										}
+										$t=$num_paginas-1;
+										echo "<li class='page-item'><a class='page-link' href='tienda.php?tipo=$tipo&id=$id&pag=$t&marcaf=$marcaf'>Ultima</a></li>";
 									?>
 							  </ul>
 							</nav>
