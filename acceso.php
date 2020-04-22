69a8d19acf4ce7990202385ba9700fb53c913a6f
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-22 09:31:49 -0500

    a
    a

--- acceso.php
+++ acceso.php
@@ -13,11 +13,11 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="OneTech shop project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-<link href="plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
-<link rel="stylesheet" type="text/css" href="styles/cart_styles.css">
-<link rel="stylesheet" type="text/css" href="styles/cart_responsive.css">
+<link rel="stylesheet" type="text/css" href="/styles/bootstrap4/bootstrap.min.css">
+<link href="/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="/styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="/styles/cart_styles.css">
+<link rel="stylesheet" type="text/css" href="/styles/cart_responsive.css">
 
 </head>
 
@@ -81,26 +81,26 @@
 
 </div>
 
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="styles/bootstrap4/popper.js"></script>
-<script src="styles/bootstrap4/bootstrap.min.js"></script>
-<script src="plugins/greensock/TweenMax.min.js"></script>
-<script src="plugins/greensock/TimelineMax.min.js"></script>
-<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
-<script src="plugins/greensock/animation.gsap.min.js"></script>
-<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
-<script src="plugins/easing/easing.js"></script>
-<script src="js/cart_custom.js"></script>
+<script src="/js/jquery-3.3.1.min.js"></script>
+<script src="/styles/bootstrap4/popper.js"></script>
+<script src="/styles/bootstrap4/bootstrap.min.js"></script>
+<script src="/plugins/greensock/TweenMax.min.js"></script>
+<script src="/plugins/greensock/TimelineMax.min.js"></script>
+<script src="/plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="/plugins/greensock/animation.gsap.min.js"></script>
+<script src="/plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="/plugins/easing/easing.js"></script>
+<script src="/js/cart_custom.js"></script>
 
 
-<script src="librerias15/jQuery-MD5-master/jquery.md5.js"></script>
+<script src="/librerias15/jQuery-MD5-master/jquery.md5.js"></script>
 
 <!--   Alertas   -->
-<script src="librerias15/swal/dist/sweetalert2.min.js"></script>
-<link rel="stylesheet" href="librerias15/swal/dist/sweetalert2.min.css">
+<script src="/librerias15/swal/dist/sweetalert2.min.js"></script>
+<link rel="stylesheet" href="/librerias15/swal/dist/sweetalert2.min.css">
 
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
-<script src="sagyc.js"></script>
+<script src="/sagyc.js"></script>
 </body>
 
 </html>
