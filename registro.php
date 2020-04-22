8964a7dbc8cc7623460ac7507748d08b8cec7a9b
stevexpika <hola@xpika.com>
2020-04-22 17:29:23 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -20,7 +20,7 @@
 <title>TIC SHOP</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="TIC-SHOP la mejor tienda de tecnologia>
+<meta name="description" content="TIC-SHOP la mejor tienda de tecnologia">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="/styles/bootstrap4/bootstrap.min.css">
 <link href="/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css" rel="stylesheet" type="text/css">
