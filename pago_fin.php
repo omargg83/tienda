f8c54b628504a382aa11682a1fdb4306193ad94e
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-22 15:03:48 -0500

    a
    a

--- pago_fin.php
+++ pago_fin.php
@@ -91,7 +91,7 @@
 		    $exist->execute();
 				$contar=$exist->rowCount();
 				if($contar>0){
-					$alma_pedido=$exist->fetch(PDO::FETCH_OBJ);
+					$alma_pedido=$exist->fetchAll(PDO::FETCH_OBJ);
 					echo "<pre>";
 						echo var_dump($alma_pedido);
 					echo "</pre>";
