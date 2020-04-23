372e4c52e2640a3183b74d3ccba2e2de72efdf52
stevexpika <hola@xpika.com>
2020-04-23 15:34:38 -0500

    Update aviso.php

--- aviso.php
+++ aviso.php
@@ -39,207 +39,78 @@
 					<div class="banner_content">
 						<h1 class="banner_text" style="color: black; font-size: 30px;">Aviso de privacidad</h1><br>
 					
-<p></p>
-<table width="100%">
-<tbody>
-<tr>
-<td>
-<p>TIC SHOP, con domicilio en Av. Tecnol&oacute;gico de Monterrey, Col. Puerta de Hierro, Pachuca de Soto, Hidalgo, C.P. 42083, ciudad PACHUCA, municipio o delegaci&oacute;n PACHUCA, C.P. 42084, en la entidad de Hidalgo, pa&iacute;s MEXICO, y portal de internet <a href="http://www.tic-shop.com.mx">www.tic-shop.com.mx</a>, es el responsable del uso y protecci&oacute;n de sus datos personales, y al respecto le informamos lo siguiente:</p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
-<p><strong>&iquest;Para qu&eacute; fines utilizaremos sus datos personales?</strong></p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
-<p>Los datos personales que recabamos de usted, los utilizaremos para las siguientes finalidades que son necesarias para el servicio que solicita:</p>
-</td>
-</tr>
-<tr>
-<td>
-<p>&middot; VALIDAR Y VERIFICAR LA TITULARIDAD DE LA TARJETA DE CREDITO O DEBITO QUE USA PARA EL PAGO</p>
-<p>&middot; ENVIAR EL PRODUCTO/MERCANCIA COMPRADO</p>
-<p>&middot; INFORMAR EL STATUS DE SU COMPRA/PEDIDO</p>
-</td>
-</tr>
-<tr>
-<td>
-<p><br /> De manera adicional, utilizaremos su informaci&oacute;n personal para las siguientes finalidades secundarias que&nbsp;<strong>no son necesarias</strong>&nbsp;para el servicio solicitado, pero que nos permiten y facilitan brindarle una mejor atenci&oacute;n:</p>
-</td>
-</tr>
-<tr>
-<td>
-<p>&middot; Mercadotecnia o publicitaria</p>
-<p>&middot; Prospecci&oacute;n comercial</p>
-</td>
-</tr>
-<tr>
-<td>
-<p>En caso de que no desee que sus datos personales se utilicen para estos fines secundarios, ind&iacute;quelo enviado un correo electr&oacute;nico a info@tecnowow.mx&nbsp;detallando:<br /> <br /> No consiento que mis datos personales se utilicen para los siguientes fines:<br /> <br /> [ ]&nbsp;Mercadotecnia o publicitaria<br /> [ ]&nbsp;Prospecci&oacute;n comercial</p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
-<p><br /> La negativa para el uso de sus datos personales para estas finalidades no podr&aacute; ser un motivo para que le neguemos los servicios y productos que solicita o contrata con nosotros.</p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
-<p><strong>&iquest;Qu&eacute; datos personales utilizaremos para estos fines?</strong></p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
-<p>Para llevar a cabo las finalidades descritas en el presente aviso de privacidad, utilizaremos los siguientes datos personales:</p>
-<p>&middot; Datos de identificaci&oacute;n</p>
-<p>&middot; Datos de contacto</p>
-<p>&middot; Datos laborales (solo en los casos de pagos con Tarjetas de cr&eacute;dito o d&eacute;bito)</p>
-</td>
-</tr>
-</tbody>
-</table>
-<table width="100%">
-<tbody>
-<tr>
-<td>
-<p><strong><br /> &iquest;C&oacute;mo puede acceder, rectificar o cancelar sus datos personales, u oponerse a su uso?</strong></p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
+<p>TIC SHOP, con domicilio en Av. Tecnol&oacute;gico de Monterrey, Col. Puerta de Hierro, Pachuca de Soto, Hidalgo, C.P 42086, Ciudad Pachuca, Municipio o delegaci&oacute;n PACHUCA, C.P. 42083, en la entidad de Hidalgo, pa&iacute;s M&eacute;xico y portal de internet www.tic-shop.com.mx es el responsable del uso y protecci&oacute;n de datos personales y al respecto le informamos lo siguiente, ha desarrollado una pol&iacute;tica de privacidad que deseamos te ayude a comprender la forma en la utilizamos, transferimos y almacenamos tu informaci&oacute;n, en conformidad con el Art&iacute;culo 16 Constitucional y los Art&iacute;culos 1 y 2 de la Ley Federal de Protecci&oacute;n de Datos Personales en Posesi&oacute;n de los Particulares.</p>
+<p>El departamento de Servicio al Cliente, es el responsable de la protecci&oacute;n y tratamiento de los datos recabados de manera directa por medios electr&oacute;nicos ya sea a trav&eacute;s del "chat" o por correo electr&oacute;nico y/o por tel&eacute;fono, y el titular podr&aacute; contactarlo en la misma direcci&oacute;n antes mencionada o al correo electr&oacute;nico: info@tic-shop.com.mx o al tel&eacute;fono 7717028040</p>
+<p><strong>&iquest;QU&Eacute; DATOS PERSONALES UTILIZAREMOS?</strong></p>
+<p>El tipo de informaci&oacute;n que recabamos consiste principalmente en:</p>
+<p>Nombre</p>
+<p>Direcci&oacute;n Postal</p>
+<p>Correo Electr&oacute;nico</p>
+<p>N&uacute;mero telef&oacute;nico</p>
+<p>N&uacute;mero telef&oacute;nico m&oacute;vil</p>
+<p>Datos Bancarios</p>
+<p>Edad</p>
+<p>G&eacute;neros</p>
+<p><strong>&iquest;PARA QU&Eacute; FINES UTILIZAREMOS TUS DATOS PERSONALES?</strong></p>
+<p>Los datos personales que recabamos de usted los ocuparemos para las siguientes finalidades, que son necesarias para el servicio que solicita.</p>
+<ul>
+<li>VALIDAR Y VERIFICAR LA TITULARIDAD DE LA TARJETA DE CREDITO O DEBITO PARA EL USO DEL PAGO</li>
+<li>ENVIAR EL PRODUCTO/MERCANCIA COMPRADO</li>
+<li>INFORMAR EL STATUS DE SU COMPRA/PEDIDO</li>
+</ul>
+<p>De manera adicional, utilizaremos su informaci&oacute;n para las siguientes finalidades secundarias que NO SON NECESARIAS para el servicio que solicitado.</p>
+<ul>
+<li>ENVIAR NOTIFICACI&Oacute;N DE OFERTAS, AVISOS Y/O MENSAJES PROMOCIONALES</li>
+<li>COMUNICACI&Oacute;N CON FINES DE MERCADOTECNIA Y PUBLICIDAD SOBRE PRODUCTOS Y SERVICIOS NUEVOS O EXISTENTES</li>
+<li>REALIZAR ENCUESTAS, ESTAD&Iacute;STICAS</li>
+</ul>
+<p>En caso de que no desee que sus datos personales se utilicen para estos fines secundarios, ind&iacute;quelo enviado un correo electr&oacute;nico a info@tic-shop.com.mx detallando:</p>
+<p>No consiento que mis datos personales se utilicen para los siguientes fines:</p>
+<ul>
+<li>Mercadotecnia o publicitaria</li>
+<li>Prospecci&oacute;n comercial</li>
+</ul>
+<p>La negativa para el uso de sus datos personales para estas finalidades no podr&aacute; ser un motivo para que le neguemos los servicios y productos que solicita o contrata con nosotros.</p>
+<p><strong>COMPROMISO: PROTECCI&Oacute;N DE INFORMACI&Oacute;N DE DATOS</strong></p>
+<p>El Responsable y/o sus Agentes conservar&aacute;n los datos personales del Titular durante el tiempo que sea necesario para procesar sus solicitudes de informaci&oacute;n, productos y/o servicios.</p>
+<p>Los datos personales recolectados se encontrar&aacute;n protegidos por medidas de seguridad administrativas, contra el da&ntilde;o, p&eacute;rdida, alteraci&oacute;n, destrucci&oacute;n o uso, acceso o tratamiento no autorizados, de conformidad con lo dispuesto en la LFPDP y la dem&aacute;s legislaci&oacute;n aplicable.</p>
+<p><strong>&iquest;C&Oacute;MO PUEDE ACCEDER, RECTIFICAR O CANCELAR SUS DATOS PERSONALES, U OPONERSE A SU USO?</strong></p>
 <p>Usted tiene derecho a conocer qu&eacute; datos personales tenemos de usted, para qu&eacute; los utilizamos y las condiciones del uso que les damos (Acceso). Asimismo, es su derecho solicitar la correcci&oacute;n de su informaci&oacute;n personal en caso de que est&eacute; desactualizada, sea inexacta o incompleta (Rectificaci&oacute;n); que la eliminemos de nuestros registros o bases de datos cuando considere que la misma no est&aacute; siendo utilizada adecuadamente (Cancelaci&oacute;n); as&iacute; como oponerse al uso de sus datos personales para fines espec&iacute;ficos (Oposici&oacute;n). Estos derechos se conocen como derechos ARCO.</p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
-<p>Para el ejercicio de cualquiera de los derechos ARCO, usted deber&aacute; presentar la solicitud respectiva a trav&eacute;s del siguiente medio:<br /> <br /> mediante correo electr&oacute;nico a info@tic-shop.com.mx</p>
-</td>
-</tr>
-</tbody>
-</table>
-<table width="100%">
-<tbody>
-<tr>
-<td>
-<p>Para conocer el procedimiento y requisitos para el ejercicio de los derechos ARCO, ponemos a su disposici&oacute;n el siguiente medio:<br /> <br /> mediante correo electr&oacute;nico a info@tic-shop.com.mx</p>
-</td>
-</tr>
-</tbody>
-</table>
-<table width="100%">
-<tbody>
-<tr>
-<td width="100%">
-<p><br /> Los datos de contacto de la persona o departamento de datos personales, que est&aacute; a cargo de dar tr&aacute;mite a las solicitudes de derechos ARCO, son los siguientes:</p>
-</td>
-</tr>
-<tr>
-<td width="100%">
-</td>
-</tr>
-<tr>
-<td width="100%">
-<p>a) Nombre de la persona o departamento de datos personales: LINDA BLANCAS ZERME&Ntilde;O<br /> b) Domicilio: Av. Tecnol&oacute;gico de Monterrey, Col. Puerta de Hierro, Pachuca de Soto, Hidalgo, C.P. 42083, ciudad PACHUCA, municipio o delegaci&oacute;n PACHUCA, C.P. 42083, en la entidad de Hidalgo, pa&iacute;s MEXICO<br /> c) Correo electr&oacute;nico: info@tic-shop.com.mx <br /> d) N&uacute;mero telef&oacute;nico: 7713762488<br /> <br /> <strong>Usted puede revocar su consentimiento para el uso de sus datos personales</strong><br /> <br /> Usted puede revocar el consentimiento que, en su caso, nos haya otorgado para el tratamiento de sus datos personales. Sin embargo, es importante que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por alguna obligaci&oacute;n legal requiramos seguir tratando sus datos personales. Asimismo, usted deber&aacute; considerar que para ciertos fines, la revocaci&oacute;n de su consentimiento implicar&aacute; que no le podamos seguir prestando el servicio que nos solicit&oacute;, o la conclusi&oacute;n de su relaci&oacute;n con nosotros.<br /> <br /> Para revocar su consentimiento deber&aacute; presentar su solicitud a trav&eacute;s del siguiente medio:<br /> <br /> correo electr&oacute;nico a la direcci&oacute;n info@tic-shop.com.mx</p>
-</td>
-</tr>
-<tr>
-<td width="100%">
-<p><br /> Para conocer el procedimiento y requisitos para la revocaci&oacute;n del consentimiento, ponemos a su disposici&oacute;n el siguiente medio:<br /> <br /> correo electr&oacute;nico a la direcci&oacute;n info@tic-shop.com.mx</p>
-</td>
-</tr>
-</tbody>
-</table>
-<table width="100%">
-<tbody>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
-<p><strong>&iquest;C&oacute;mo puede limitar el uso o divulgaci&oacute;n de su informaci&oacute;n personal?</strong></p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
-<p>Con objeto de que usted pueda limitar el uso y divulgaci&oacute;n de su informaci&oacute;n personal, le ofrecemos los siguientes medios:<br /> <br /> correo electr&oacute;nico a la direcci&oacute;n info@tic-shop.com.mx</p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-</tbody>
-</table>
-<table width="100%">
-<tbody>
-<tr>
-<td>
-<p><strong>El uso de tecnolog&iacute;as de rastreo en nuestro portal de internet</strong><br /> <br /> Le informamos que en nuestra p&aacute;gina de internet utilizamos cookies, web beacons u otras tecnolog&iacute;as, a trav&eacute;s de las cuales es posible monitorear su comportamiento como usuario de internet, as&iacute; como brindarle un mejor servicio y experiencia al navegar en nuestra p&aacute;gina. Los datos personales que recabamos a trav&eacute;s de estas tecnolog&iacute;as, los utilizaremos para los siguientes fines:<br /> <br /> Ofrecer productos y servicios m&aacute;s id&oacute;neas de acuerdo a sus b&uacute;squedas y necesidades<br /> <br /> Los datos personales que obtenemos de estas tecnolog&iacute;as de rastreo son los siguientes:<br /> <br /> Regi&oacute;n en la que se encuentra el usuario<br /> Tipo de navegador del usuario<br /> Tipo de sistema operativo del usuario<br /> P&aacute;ginas web visitadas por un usuario<br /> B&uacute;squedas realizadas por un usuario<br /> <br /> </p>
-</td>
-</tr>
-</tbody>
-</table>
-<table width="100%">
-<tbody>
-<tr>
-<td>
-</td>
-</tr>
-</tbody>
-</table>
-<table width="100%">
-<tbody>
-<tr>
-<td width="100%">
-<p><strong><br /> &iquest;C&oacute;mo puede conocer los cambios en este aviso de privacidad?</strong></p>
-</td>
-</tr>
-<tr>
-<td>
-</td>
-</tr>
-<tr>
-<td>
-<p>El presente aviso de privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; de nuestras propias necesidades por los productos o servicios que ofrecemos; de nuestras pr&aacute;cticas de privacidad; de cambios en nuestro modelo de negocio, o por otras causas.<br /> <br /> Nos comprometemos a mantenerlo informado sobre los cambios que pueda sufrir el presente aviso de privacidad, a trav&eacute;s de: nuestra pagina web.<br /> <br /> El procedimiento a trav&eacute;s del cual se llevar&aacute;n a cabo las notificaciones sobre cambios o actualizaciones al presente aviso de privacidad es el siguiente:<br /> <br /> Consultando el Aviso de Privacidad de nuestra pagina web</p>
-</td>
-</tr>
-</tbody>
-</table>
+<p>Para el ejercicio de cualquiera de los derechos ARCO, usted deber&aacute; presentar la solicitud respectiva a trav&eacute;s del siguiente medio:</p>
+<p>Mediante correo electr&oacute;nico a info@tic.shop.com.mx</p>
+<p>Para conocer el procedimiento y requisitos para el ejercicio de los derechos ARCO, ponemos a su disposici&oacute;n el siguiente medio:</p>
+<p>Mediante correo electr&oacute;nico a info@tic.shop.com.mx</p>
+<p>En el caso de las solicitudes de rectificaci&oacute;n de datos personales, el Titular deber&aacute; tambi&eacute;n indicar las modificaciones a realizarse y aportar la documentaci&oacute;n que sustente su petici&oacute;n.</p>
+<p>Para la petici&oacute;n del formato, recepci&oacute;n y respuesta de las solicitudes para ejercer sus derechos <strong>ARCO,</strong> la revocaci&oacute;n de su consentimiento y los dem&aacute;s derechos previstos en la LFPDP ponemos a su disposici&oacute;n los siguientes medios:</p>
+<ol>
+<li>a) Nombre de la persona o departamento de datos personales: <strong>JUAN LUIS VITE VIVANCO</strong></li>
+<li>b) Domicilio: <strong> Tecnol&oacute;gico de Monterrey, Col. Puerta de Hierro, Pachuca de Soto, Hidalgo, C.P 42086, Ciudad Pachuca, en la entidad de Hidalgo, pa&iacute;s MEXICO</strong></li>
+<li>c) Correo electr&oacute;nico: <strong>info@tic-shop.com.mx</strong></li>
+<li>d) N&uacute;mero telef&oacute;nico: <strong>7717028040</strong></li>
+</ol>
+<p><strong>USTED PUEDE REVOCAR SU CONSENTIMIENTO PARA EL USO DE SUS DATOS PERSONALES</strong></p>
+<p>Usted puede revocar el consentimiento que, en su caso, nos haya otorgado para el tratamiento de sus datos personales. Sin embargo, es importante que tenga en cuenta que no en todos los casos podremos atender su solicitud o concluir el uso de forma inmediata, ya que es posible que por alguna obligaci&oacute;n legal requiramos seguir tratando sus datos personales. Asimismo, usted deber&aacute; considerar que para ciertos fines, la revocaci&oacute;n de su consentimiento implicar&aacute; que no le podamos seguir prestando el servicio que nos solicit&oacute;, o la conclusi&oacute;n de su relaci&oacute;n con nosotros.</p>
+<p>Para revocar su consentimiento deber&aacute; presentar su solicitud a trav&eacute;s del siguiente medio:</p>
+<p>Correo electr&oacute;nico a la direcci&oacute;n info@tic-shop.com.mx</p>
+<p>Para conocer el procedimiento y requisitos para la revocaci&oacute;n del consentimiento, ponemos a su disposici&oacute;n el siguiente medio:</p>
+<p>Correo electr&oacute;nico a la direcci&oacute;n info@tic-shop.com.mx</p>
+<p><strong>&iquest;C&Oacute;MO PUEDE LIMITAR EL USO O DIVULGACI&Oacute;N DE SU INFORMACI&Oacute;N PERSONAL?</strong></p>
+<p>Con objeto de que usted pueda limitar el uso y divulgaci&oacute;n de su informaci&oacute;n personal, le ofrecemos los siguientes medios:</p>
+<p>Correo electr&oacute;nico a la direcci&oacute;n info@tic-shop.com.mx</p>
+<p>El uso de tecnolog&iacute;as de rastreo en nuestro portal de internet</p>
+<p>Le informamos que en nuestra p&aacute;gina de internet utilizamos cookies, web beacons u otras tecnolog&iacute;as, a trav&eacute;s de las cuales es posible monitorear su comportamiento como usuario de internet, as&iacute; como brindarle un mejor servicio y experiencia al navegar en nuestra p&aacute;gina. Los datos personales que recabamos a trav&eacute;s de estas tecnolog&iacute;as, los utilizaremos para los siguientes fines:</p>
+<p>Ofrecer productos y servicios m&aacute;s id&oacute;neos de acuerdo a sus b&uacute;squedas y necesidades</p>
+<p>Los datos personales que obtenemos de estas tecnolog&iacute;as de rastreo son los siguientes:</p>
+<p>Regi&oacute;n en la que se encuentra el usuario</p>
+<p>Tipo de navegador del usuario</p>
+<p>Tipo de sistema operativo del usuario</p>
+<p>P&aacute;ginas web visitadas por un usuario</p>
+<p>B&uacute;squedas realizadas por un usuario</p>
+<p><strong>&iquest;C&Oacute;MO PUEDE CONOCER LOS CAMBIOS EN ESTE AVISO DE PRIVACIDAD?</strong></p>
+<p>El presente aviso de privacidad puede sufrir modificaciones, cambios o actualizaciones derivadas de nuevos requerimientos legales; de nuestras propias necesidades por los productos o servicios que ofrecemos; de nuestras pr&aacute;cticas de privacidad; de cambios en nuestro modelo de negocio, o por otras causas.</p>
+<p>Nos comprometemos a mantenerlo informado sobre los cambios que pueda sufrir el presente aviso de privacidad, a trav&eacute;s de: nuestra p&aacute;gina web.</p>
+<p>El procedimiento a trav&eacute;s del cual se llevar&aacute;n a cabo las notificaciones sobre cambios o actualizaciones al presente aviso de privacidad es el siguiente:</p>
+<p>Consultando el Aviso de Privacidad de nuestra p&aacute;gina web</p>
 
 					</div>
 				</div>
