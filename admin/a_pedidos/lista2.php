e14c901f53e02cd1c5fe4452fe74c45848c302cb
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-23 14:58:29 -0500

    a
    a

--- admin/a_pedidos/lista2.php
+++ admin/a_pedidos/lista2.php
@@ -22,9 +22,13 @@
 				if (count($pd)>0){
 					foreach($pd as $key){
 						echo "<tr id='".$key['id']."' class='edit-t'>";
+
+						echo "<td>";
 						echo "<div class='btn-group'>";
 							echo "<button class='btn btn-outline-secondary btn-sm' id='edit_comision' title='Editar' data-lugar='a_pedidos/editar'><i class='fas fa-pencil-alt'></i></button>";
 						echo "</div>";
+						echo "</td>";
+
 						echo "<td>".$key["id"]."</td>";
 						echo "<td>".$key["idprod"]."</td>";
 						echo "<td>".$key["clave"]."</td>";
