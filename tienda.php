d35c3d9560200908e3fa52d3ae8df5fe7f52358a
stevexpika <hola@xpika.com>
2020-04-24 16:25:44 -0500

    Update tienda.php

--- tienda.php
+++ tienda.php
@@ -431,14 +431,13 @@
 
 <script type="text/javascript">
 
-$(window).load(function() {
   $('img').each(function() {
     if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
       // image was broken, replace with your new image
        $(this).remove();
     }
   });
-});;
+
 
 </script>
 
