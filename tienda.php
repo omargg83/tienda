d55d544a5aa4e29532e3624a298c72adb02138ed
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-28 10:06:31 -0500

    a
    a

--- tienda.php
+++ tienda.php
@@ -308,7 +308,15 @@
 										<img src='/".$db->doc.$key->img.$a."' alt='' style='max-width:65px;max-height:65px' ></div>
 										<div class='product_content'>
 											<div class='product_price'>".moneda($preciof)."</div>
-											<div class='product_name'><div><a href='/producto/".$key->clave."' tabindex='0'>".$key->nombre."</a></div></div>
+											<div class='product_name' style='display: block;
+										    display: -webkit-box;
+										    height: $font-size * $line-height * $lines;
+										    font-size: $font-size;
+										    line-height: $line-height;
+										    -webkit-line-clamp: $lines;
+										    -webkit-box-orient: vertical;
+										    overflow: hidden;
+										    text-overflow: ellipsis;'><div><a href='/producto/".$key->clave."' tabindex='0'>".$key->nombre."</a></div></div>
 										</div>
 										<div class='product_fav' onclick='wish(".$key->id.")'><i class='fas fa-heart'></i></div>
 										<ul class='product_marks'>
