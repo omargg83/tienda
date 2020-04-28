0ae65f4fd8738b6b0fa4aaa46dbe8c7fe93e0800
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-28 09:35:18 -0500

    a
    a

--- tienda.php
+++ tienda.php
@@ -425,25 +425,6 @@
 
 </script>
 
-<script type="text/javascript">
-
-$( document ).ready(function() {
-
-function imgrm() {
-	$('img').each(function() {
-	    if (!this.complete || typeof this.naturalWidth == "undefined" || this.naturalWidth == 0) {
-	      // image was broken, replace with your new image
-	      $(this).parent().parent().parent().remove();
-	    }
-	  });
-	};
-	setInterval(imgrm, 3000)
-	$('.product_item').removeAttr("style");
-	$('.product_grid').removeAttr("style");
-
- });
-
-</script>
 
 
 
