9107de6941a1e9fe1cedf149fe1867b42df916c9
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-27 23:13:26 -0500

    a
    a

--- demo3.php
+++ demo3.php
@@ -1,27 +1,4 @@
 <?php
-function myscandir($dir, $exp, $how='name', $desc=0)
-{
-  $r = array();
-  $dh = @opendir($dir);
-  if ($dh) {
-      while (($fname = readdir($dh)) !== false) {
-          if (preg_match($exp, $fname)) {
-              $stat = stat("$dir/$fname");
-              $r[$fname] = ($how == 'name')? $fname: $stat[$how];
-          }
-      }
-      closedir($dh);
-      if ($desc) {
-          arsort($r);
-      }
-      else {
-          asort($r);
-      }
-  }
-  return(array_keys($r));
-}
 
-$r = myscandir('admin/a_imagen/', '/^article[0-9]{4}\.txt$/i', 'ctime', 1);
-print_r($r);
 
 ?>
