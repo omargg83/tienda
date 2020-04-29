36463a22c72cdf09fa9723592cfd72b27f116fc7
Omar <51249360+omargg83@users.noreply.github.com>
2020-04-28 21:34:01 -0500

    a
    a

--- admin/a_jobs/001_imagen.php
+++ admin/a_jobs/001_imagen.php
@@ -21,20 +21,8 @@
   }
   $db = new Imagen();
 
-  $sql="select * from productos where  imagen_exist=2 and interno=0 limit 10";
+  $sql="select * from productos where imagen_exist=0 and interno=0 limit 20";
   $stmt= $db->dbh->query($sql);
-
-  /*
-
-    $url=$key['imagen'];
-     echo "<br>".$url;
-
-     if (!file_exists("../a_imagen/".$nombre)) {
-       $imagen = file_get_contents($url);
-       file_put_contents("../a_imagen/".$nombre, $imagen);
-     }
-  */
-
 
   foreach($stmt as $key){
     echo "<hr>";
@@ -56,16 +44,12 @@
         $sth2->execute();
       }
       else{
-        //$imagen = file_get_contents($url);
-        //echo "contenido:".file_put_contents("../a_imagen/".$nombre, $imagen);
-
         $sql="update productos set imagen_exist=2, img=:nombre where id=:id";
         $sth2 = $db->dbh->prepare($sql);
         $sth2->bindValue(':id',$key['id']);
         $sth2->bindValue(':nombre', $nombre);
         $sth2->execute();
         echo "<br>error";
-
       }
     }
     else{
@@ -75,31 +59,7 @@
       $sth2->bindValue(':id',$key['id']);
       $sth2->bindValue(':nombre', $nombre);
       $sth2->execute();
-
     }
-
-      /*
-      $nombre=trim(basename(trim($key['imagen'])));
-
-      $imagen = file_get_contents($url);
-
-      $data = file_get_contents($almax->imagen);
-      $img = imagecreatefromstring($data);
-
-      $resp=file_put_contents("../a_imagen/".$nombre, $imagen);
-
-
-      echo "error de imagen 1";
-      $img = imagecreatefromstring($imagen);
-      if(imagejpeg($img,"../a_imagen/".$nombre)){
-        $sth2->bindValue(':nombre', $nombre);
-        $sth2->bindValue(':id',$key['id']);
-        $sth2->execute();
-      }
-      else{
-        echo "error de imagen 2";
-      }
-      */
   }
   echo "<br>Fin";
 ?>
