dfc338b5c271ffa2f3c07a56faaab3bc44c5760e
Omar <51249360+omargg83@users.noreply.github.com>
2020-06-11 01:44:03 -0500

    Update control_db.php

--- TIC_CONTROL/control_db.php
+++ TIC_CONTROL/control_db.php
@@ -25,7 +25,10 @@
 				$ip=self::getRealIP();
         $clave=md5("tic%pika_$%&/()=".$ip);
 
-				if(!isset($_SESSION['idsess']) and $_SESSION['idsess']!=$clave){
+				if(isset($_SESSION['idsess']) and $_SESSION['idsess']==$clave){
+
+				}
+				else{
 					die();
 				}
 
