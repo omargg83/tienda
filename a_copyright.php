570e62cca642f1ff52f239b64ed2484998ab3d2b
stevexpika <hola@xpika.com>
2020-07-16 16:17:07 -0500

    Update a_copyright.php

--- a_copyright.php
+++ a_copyright.php
@@ -17,4 +17,14 @@
         </div>
     </div>
   </div>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-172841150-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-172841150-1');
+</script>
+
 </div>
